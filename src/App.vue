d89113194ef36fff515f1c9f2e4abb8173a2b4d7
Emma-Chantal Engledall <143702440+emmaengledall@users.noreply.github.com>
2025-01-14 00:21:18 +0100

    firstUpdate

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<template>
+  <div class="bg">
+  
+  <header>
+    
+
+    <div class="wrapper">
+      
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+
+      </nav>
+    </div>
+
+
+  </header>
+
+  <RouterView />
+
+</div>
+</template>
+
+
+
+<style scoped>
+
+.bg {
+  background: linear-gradient(180deg, #ffffff 0%, #FF9D2E 100%);
+  height: 100vh; /* Full viewport height */
+}
+
+
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+    <div class="login">
+      <h1>This is a login page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
